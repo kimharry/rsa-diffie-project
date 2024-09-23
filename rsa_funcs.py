bbfe0d734eb6d7f67588860f498e3011695cb84a
Harry Kim <harry@Harryui-noteubug.local>
2024-09-23 12:40:12 +0900

    rsa_funcs.py implemented

---
+++ rsa_funcs.py
@@ -0,0 +1,65 @@
+import random
+import math
+
+def is_prime(n):
+    if n == 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def rsa_keygen():
+    p = 0
+    q = 0
+    while not is_prime(p):
+        p = random.randint(100, 1000)
+    while not is_prime(q):
+        q = random.randint(100, 1000)
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    e = 0
+    while gcd(e, phi) != 1:
+        e = random.randint(2, phi - 1)
+    d = 0
+    while (d * e) % phi != 1:
+        d += 1
+    return (n, e, d)
+
+def rsa_encrypt(n, e, m):
+    c = []
+    for i in m:
+        c.append((ord(i) ** e) % n)
+    return c
+
+def rsa_decrypt(n, d, c):
+    m = ""
+    for i in c:
+        m += chr((i ** d) % n)
+    return m
+
+def verify_rsa_keypair(p, q, e, d):
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    if (p * q) % n == 0 and (p - 1) * (q - 1) % phi == 0:
+        if (e * d) % phi == 1:
+            return True
+    return False
+
+if __name__ == '__main__':
+    n, e, d = rsa_keygen()
+    print(n, e, d)
+    m = "Hello world!"
+    c = rsa_encrypt(n, e, m)
+    print(c)
+    m = rsa_decrypt(n, d, c)
+    print(m)
